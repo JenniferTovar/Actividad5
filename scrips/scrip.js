0cce3b8720cc077c25e4524e1de89c9cb5f833a1
Jenifer Alejandra Tovar barrero <jenifer.alejandratovar13@gmail.com>
2021-01-14 17:27:53 -0500

    Se carga actividad 5

---
+++ scrips/scrip.js
@@ -0,0 +1,16 @@
+function cambio1() {
+    $(this).css('background', 'yellow')
+}
+
+function cambio2() {
+    $(this).css('background', 'blue')
+}
+
+$('.definicion').mouseover(function(){
+    $(this).css('background', 'brown')
+        })
+
+        $('.texto').mouseover(function(){
+            $(this).css('background', 'yellow')
+             })
+
